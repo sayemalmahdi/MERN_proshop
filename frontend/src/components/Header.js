05cde005197b9c82df0195ea18b6d32206707408
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-02-06 19:33:55 +0600

    Implementing React Router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">MERN ProShop</Navbar.Brand>
+          <LinkContainer to='/'>
+              <Navbar.Brand>ProShop</Navbar.Brand>
+            </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-              <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
+            <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const ProductScreen = () => {
+  return (
+    <div>
+      Product &nbsp;
+      <Link to="/">Home page</Link>
+    </div>
+  )
+}
+
+export default ProductScreen
